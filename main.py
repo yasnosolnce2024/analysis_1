479d4b1b61758e605b751f6174d5175c733f0fb1
yasnosolnce2024 <pomogaika51@gmail.com>
2024-05-25 16:51:56 +0700

    add code 2

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ df = pd.read_csv('World-happiness-report-2024.csv')
 print(df.head())
 print(df.info())
 print(df.describe())
+
+df = pd.read_csv('dz.csv')
+average = df["Salary"].mean()
+
+print("Средняя зарплата:", average)
+
+print(df.head())
